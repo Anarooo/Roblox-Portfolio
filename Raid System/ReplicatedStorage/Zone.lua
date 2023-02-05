3fadd6c60d641ae26876635cb200eeeead46c8c7
Anaro <75759806+Anarooo@users.noreply.github.com>
2023-02-05 14:28:51 +0000

    Create Zone.lua

---
+++ Raid System/ReplicatedStorage/Zone.lua
@@ -0,0 +1,4 @@
+https://devforum.roblox.com/t/zoneplus-v320-construct-dynamic-zones-and-effectively-determine-players-and-parts-within-their-boundaries/1017701
+
+external module used for better hit detection with points using 'Touched' was causing constant issues where the script would duplicate, or fail to end even when using debounces
+
